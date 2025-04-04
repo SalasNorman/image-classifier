e92219caf9d17c91baafa479fb3af7add71a2c2d
SalasNorman <106792006+SalasNorman@users.noreply.github.com>
2025-04-04 02:24:00 +0000

    remove files

--- venv/etc/profile.d/mamba.sh
+++
@@ -1,8 +0,0 @@
-export MAMBA_ROOT_PREFIX="/workspaces/image-classifier/venv"
-__mamba_setup="$("/workspaces/image-classifier/venv/bin/mamba" shell hook --shell posix 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias mamba="/workspaces/image-classifier/venv/bin/mamba"  # Fallback on help from mamba activate
-fi
-unset __mamba_setup
